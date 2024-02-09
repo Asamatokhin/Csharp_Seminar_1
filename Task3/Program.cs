03df4b031b9a80e55f5d2e4ce4215232ad474c1d
Александр <asamatokhin@gmail.com>
2024-02-09 14:49:51 +0300

    добавлено решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3
+// На вход принимаем число number и выводим, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
