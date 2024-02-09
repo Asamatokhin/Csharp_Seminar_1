9eae689804269fe901fe0ac3af25cab480f7866e
Александр <asamatokhin@gmail.com>
2024-02-09 14:53:30 +0300

    добавлено решение четвертой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4
+// На вход принимаем число (number), а на выходе показываем все чётные числа от 1 до number(включительно),
+// после каждого числа должен быть знак пробела.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if (number > 1)
+{
+    while (evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
